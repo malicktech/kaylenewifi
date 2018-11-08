007129c38fa72bae5cac266d8dc2fb511d351421
Malick DIOP <diop-malick@hotmail.fr>
2018-11-08 23:30:42 +0000

    Implement Entities auditing

---
+++ src/main/java/com/kaylene/wifi/config/audit/EntityAuditEventConfig.java
@@ -0,0 +1,14 @@
+package com.kaylene.wifi.config.audit;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EntityAuditEventConfig implements BeanFactoryAware {
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        EntityAuditEventListener.setBeanFactory(beanFactory);
+    }
+}
